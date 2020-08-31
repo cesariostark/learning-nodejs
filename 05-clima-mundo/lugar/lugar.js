158c64563308dd530183cb58180ffdd697e89356
Cesario <cipoca21@gmail.com>
2020-08-31 02:37:52 -0400

    Añado parte 1 - app clima

---
+++ 05-clima-mundo/lugar/lugar.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(direccion, ciudad) => {
+    const encodeUrl = encodeURI(direccion)
+    const encodeUrl2 = encodeURI(ciudad)
+    console.log(encodeUrl);
+    const instance = axios.create({
+        baseURL: `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=${encodeUrl}&namePrefix=${encodeUrl2}`,
+        headers: { 'X-RapidAPI-key': '22a0ea7d8bmsh5cc9776530e0f74p18ae9bjsna52a936b558a' }
+    });
+
+
+    const resp = await instance.get()
+
+    if (resp.data.data[0] === 0) {
+        throw new Error('no hay resultados')
+    }
+
+    const data = resp.data.data[0];
+    const pais = data.countryCode;
+    const city = data.city;
+    const lat = data.latitude;
+    const lng = data.longitude;
+
+    return {
+        pais,
+        city,
+        lat,
+        lng
+    }
+}
+
+module.exports = {
+    getLugarLatLng
+}
